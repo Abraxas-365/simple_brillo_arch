cf877f77c6fa81986ea89c3a4da468a966901e70
Abraxas-365 <luisfmiranda8@gmail.com>
2020-05-24 15:11:50 -0500

    beta

---
+++ brillo.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/MarinX/keylogger"
+	"github.com/sirupsen/logrus"
+	"os/exec"
+	"strconv"
+)
+
+func main() {
+
+	keyboard := keylogger.FindKeyboardDevice()
+
+	logrus.Println("Found a keyboard at", keyboard)
+
+	k, err := keylogger.New(keyboard)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+	defer k.Close()
+
+	poder := 1800
+	poders := " "
+	events := k.Read()
+	for even := range events {
+		switch even.Type {
+
+		case keylogger.EvKey:
+
+			if even.KeyString() == "F1" {
+				poder = poder - 200
+				poders = strconv.Itoa(poder)
+				exec.Command("/bin/sh", "-c", "echo "+poders+" > /sys/class/backlight/intel_backlight/brightness").CombinedOutput()
+			}
+			if even.KeyString() == "F2" {
+				poder = poder + 200
+				poders = strconv.Itoa(poder)
+				exec.Command("/bin/sh", "-c", "echo "+poders+" > /sys/class/backlight/intel_backlight/brightness").CombinedOutput()
+
+			}
+
+			break
+		}
+	}
+}
